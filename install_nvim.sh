64940adcd5226bddfaba23e218545deb51dc6cde
iankim <ian.kim@neuralink.com>
2025-04-24 13:19:30 -0500

    added fzf

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y git cmake gettext ripgrep fd-find
+sudo apt-get install -y git cmake gettext ripgrep fd-find fzf
 git clone https://github.com/neovim/neovim
 cd neovim && git checkout release-0.10 && make CMAKE_BUILD_TYPE=Release
 sudo make install
